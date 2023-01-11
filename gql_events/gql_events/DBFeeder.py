50f35b8db179e925b44cfde28050cbc785ee9607
Megalekk <filip.zindler@seznam.cz>
2023-01-11 21:16:12 +0100

    feederupd

--- gql_events/gql_events/DBFeeder.py
+++ gql_events/gql_events/DBFeeder.py
@@ -27,51 +27,37 @@ def singleCall(asyncFunc):
 #
 ###########################################################################################################################
 
-data = {
-    'events': [
-        {'id':'73dda931-1629-4193-963a-c55397b0a706','name':'Slavnostni nastup',
-         'start': datetime.datetime(), 'end': datetime.datetime() + datetime.timedelta(hours=1,minutes=30),
-         'eventType':'nastup',
-         'location':'',
-         'lessons':[
-                {'id':''}
-            ],
-         'groups':[
-                {'id':''}
-            ],
-         'participants':[
-                {'id':'0cd946af-b840-4b6f-ae4e-2c4029e531a6','nejakyAtribut':'random','user_id':''}  #user_id je z jineho kontejneru
-            ]
-        },
-
-        {'id':'d3058a19-5234-4f36-a151-395f07c4eb30','name':'Beseda s Dekanem',
-         'start': datetime.datetime(), 'end': datetime.datetime() + datetime.timedelta(hours=1,minutes=30),
-         'eventType':'beseda',
-         'location':'',
-         'lessons':[
-                {'id':''}
-            ],
-         'groups':[
-                {'id':''}
-            ],
-         'participants':[
-                {'id':'','nejakyAtribut':'random','user_id':''}
-            ]
-        },
-
-        {'id':'90749da9-9a42-47f6-b4a8-aff8b15f4a27','name':'Sportovni den',
-         'start': datetime.datetime(), 'end': datetime.datetime() + datetime.timedelta(hours=1,minutes=30), #do 16:00
-         'eventType':'sport',
-         'location':'',
-         'lessons':[
-                {'id':''}
-            ],
-         'groups':[
-                {'id':''}
-            ],
-         'participants':[
-                {'id':'','nejakyAtribut':'random','user_id':''}
-            ]
+@cache
+def DetermineEvents():
+    data = [
+        {'id':'73dda931-1629-4193-963a-c55397b0a706','name':'',
+            'start': datetime.datetime(), 'end': datetime.datetime() + datetime.timedelta(hours=1,minutes=30),
+            'capacity':'',
+            'comment':''
         }
     ]
-}
+    return data
+
+@cache
+def DetermineEventTypes():
+    data = [
+        {'id': '794b4c17-6fde-4a84-bea0-bddbb0632ec4', 'name': 'Cviceni'},
+        {'id': 'c9a55358-e1c0-4873-abea-43a72516e282', 'name': 'Prednaska'},
+        {'id': 'ecd54144-ba57-4292-abc7-502d64c209a0', 'name': 'Seminar'},
+        {'id': '14f68158-aeab-4e76-862e-fd2079892a59', 'name': 'Slavnostni nastup'},
+        {'id': '9387085c-bca1-4470-88b4-2e72241e7de6', 'name': 'Sportovni den'},
+        {'id': '29f1c23b-c74a-4070-ac54-658672129699', 'name': 'Beseda'},
+        {'id': 'a707696e-a051-4876-a995-899f1f27d4d9', 'name': 'Zkouska'},
+        {'id': '8e599414-99d8-43fd-bfa6-51ca9a6a2426', 'name': 'Zapoctovy tes'},
+        {'id': '61e7d4e4-2bb4-4ce0-8065-7ce2fa61abb4', 'name': 'Jina udalost'},
+        {'id': 'b71bac7a-536e-4bac-8aff-687a65127eaa', 'name': 'Samostudium'}, 
+    ]
+    return data
+
+@cache
+def DetermineLocations():
+    data = [
+        {'id': '812d1598-4771-4d38-a52b-4cfeff075488', 'name': 'KJB'},
+        {'id': '090d7268-9661-4594-99db-e70c3a187e2d', 'name': 'Sumavska'},
+        {'id': '0a5ce699-0763-4e6f-8278-9f19b73d50fd', 'name': 'Kounicova'},
+    ]
